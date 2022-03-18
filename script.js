2b3052615b30871dfa57589e23ee903fab6abfd2
Rohit <halixh39@gmail.com>
2022-03-18 18:58:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
+const imgs = document.querySelectorAll("img");
+imgs.forEach(Image => {
+    Image.addEventListener("click", e => {
+        lightbox.classList.add("active");
+        const img = document.createElement("img");
+        img.src = Image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img)
+    });
+})
+lightbox.addEventListener("click", e => {
+    if (e.target !== e.currentTarget) return
+
+    lightbox.classList.remove("active");
+})
